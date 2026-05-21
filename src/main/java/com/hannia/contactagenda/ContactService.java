2aec48ea09f619f0fcc3ebf1f798d26939c3c8af
hannjimnz <hannjimnz@gmail.com>
2026-05-20 22:35:13 -0600

    Implemented basic CRUD for contacts

--- src/main/java/com/hannia/contactagenda/ContactService.java
+++ src/main/java/com/hannia/contactagenda/ContactService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ContactService {
     private List <Contact> contacts = new ArrayList<>();
 
