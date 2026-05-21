c7909bf9e7aeb9e735d06f4db9f6122527e68625
hannjimnz <hannjimnz@gmail.com>
2026-05-20 21:04:12 -0600

    Add and create ContactService

---
+++ src/main/java/com/hannia/contactagenda/ContactService.java
@@ -0,0 +1,19 @@
+package com.hannia.contactagenda;
+
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactService {
+    private List <Contact> contacts = new ArrayList<>();
+
+    public List<Contact> getAllContacts() {
+        return contacts;
+    }
+    public void addContact(Contact contact){
+        contacts.add(contact);
+    }
+    public void deleteContact(String name){
+        contacts.removeIf(c -> c.getName().equalsIgnoreCase(name));
+    }
+}
