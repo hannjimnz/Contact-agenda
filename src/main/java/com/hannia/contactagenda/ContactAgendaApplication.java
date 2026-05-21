54560b72fcac4de36552d6ed71332645aca299e1
hannjimnz <hannjimnz@gmail.com>
2026-05-20 20:11:34 -0600

    Initilize spring Boot project

---
+++ src/main/java/com/hannia/contactagenda/ContactAgendaApplication.java
@@ -0,0 +1,16 @@
+package com.hannia.contactagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactAgendaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ContactAgendaApplication.class, args);
+
+
+    }
+
+}
---
+++ src/test/java/com/hannia/contactagenda/ContactAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hannia.contactagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactAgendaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
